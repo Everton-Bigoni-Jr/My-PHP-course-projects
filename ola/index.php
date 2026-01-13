6f1c7844d32ac497d5d9f6355982a445f860e37d
Everton Bigoni Junior <evertonbgjunior@gmail.com>
2026-01-13 15:40:15 -0400

    Add files via upload

---
+++ ola/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá mundo!</title>
+</head>
+<body>
+
+<h1>👑 Jesus is King 👑</h1>
+<h2>Code by - @evertin_bg</h2>
+<h3>👉 My <a href="https://github.com/Everton-Bigoni-Jr" target="_blank">GitHub</a></h3>
+<hr>
+
+
+    <h1>
+        <?php
+            echo "Olá Mundo"
+        ?>
+    </h1>
+</body>
+</html>
