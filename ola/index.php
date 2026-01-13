edcb491f3fbe3b07d37a56e8314bc1b04d7d0385
Everton Bigoni Junior <evertonbgjunior@gmail.com>
2026-01-13 16:39:44 -0400

    Update index.php

--- ola/index.php
+++ ola/index.php
@@ -7,16 +7,19 @@
 </head>
 <body>
 
-<h1>👑 Jesus is King 👑</h1>
-<h2>Code by - @evertin_bg</h2>
-<h3>👉 My <a href="https://github.com/Everton-Bigoni-Jr" target="_blank">GitHub</a></h3>
-<hr>
+<!--HTML code--> 
+    <h1>👑 Jesus is King 👑</h1>
+    <h2>Code by - @evertin_bg</h2>
+    <h3>👉 My <a href="https://github.com/Everton-Bigoni-Jr" target="_blank">GitHub</a></h3>
+    <hr>
+<!--HTML code-->
 
-
+<!--PHP code-->
     <h1>
         <?php
-            echo "Olá Mundo"
+            echo "Olá Mundo";
         ?>
     </h1>
+<!--PHP code-->
 </body>
 </html>
